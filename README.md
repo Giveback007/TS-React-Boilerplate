e10afd9a15565e9a24512ee01dc0097913c3be9b
Dovydas Stirpeika <dovy91@gmail.com>
2018-11-01 19:02:54 -0500

    fully working

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+Typescript & React boilerplate creator with Redux & live-reloading that keeps app state on changes.
+
 All that should be required is to have node, npm, and git installed this boilerplate will take care of the rest.
+
+Command: `node run` or open the `create-project.bat` file.
+
+This project only works on windows.
+
+
--- run.js
+++ run.js
@@ -1,59 +1,38 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
-const readline = require('readline');
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-const question = (q) => new Promise((res) => rl.question(q, (answer) => { res(answer); rl.close(); }));
+const appUtil = require('./app-util');
+const copyFiles = require('./copy').copyFiles;
 
-// COPY
-// fs.createReadStream('test.log').pipe(fs.createWriteStream('newLog.log'));
-// COPY
-
-const testName = (input) => /^[a-z0-9_-]+$/.test(input);
-const toCapName = (str) => str.replace(/_-/g, " ").split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
-
-async function askProjectName() {
-    const name = await question('Project name? (valid folder name, lowercase, and no spaces) ');
-
-    if (!testName(name)) {
-        console.log(`Invalid project name: "${name}"`);
-        process.exit(1);
-    } else return name
-}
-
-async function run() {
-    const projectName = await askProjectName();
+// RUN:
+! async function() {
+    const projectName = await appUtil.askProjectName();
     fs.mkdirSync(`../${projectName}`);
 
     const data = JSON.parse(fs.readFileSync(`./data.json`, { encoding: 'utf-8' }));
     data.name = projectName;
 
     const readme = 
-        `# ${toCapName(projectName)}
+    `# ${appUtil.toCapName(projectName)}`
+    + '\n\nThe project can be viewed here:'
+    + '\n\n[Link-To-Live-Project](https://)'
+    + '\n\n### Goals:'
+    + '\n* Do something'
 
-        The project can be viewed here:
+    const git = 'git init && git add -A && git commit -m "Initial commit"'
 
-        [Link-To-Live-Project](https://)
-
-        ### Goals:
-        * Do something`
-
-    console.log('### Creating Files ###');
+    console.log('\n### Creating Files ###');
     fs.appendFileSync(`../${projectName}/package.json`, JSON.stringify(data));
     fs.appendFileSync(`../${projectName}/README.md`, readme);
-    fs.appendFileSync(`../${projectName}/install.bat`, 'npm install && exit');
+    fs.appendFileSync(`../${projectName}/install.bat`, `${git} && npm install && code . && exit`);//  
+    copyFiles('./copy-files', `../${projectName}`);
 
-    console.log('### Installing NPM Packages ###');
+    console.log('\n### Installing NPM Packages ###');
     await exec(`cd ../${projectName} && start install.bat`);
     fs.unlinkSync(`../${projectName}/install.bat`);
 
-    console.log('DONE');
-}
-
-run();
-
-
-// fs.createReadStream('test.log').pipe(fs.createWriteStream('newLog.log'));
-
-// fs.readdirSync('./files') //?
+    console.log(`\n### Project "${appUtil.toCapName(projectName)}" Created ###`);
+    console.log('\n### Run the project with "npm start"\n');
+    process.exit(0);
+}();
